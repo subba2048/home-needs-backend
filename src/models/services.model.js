55ab1ada0f4f1f975a0a3a105639316d6b12298d
Subba <Subba@gmail.com>
2020-04-16 22:08:58 -0500

    changes to services module

--- src/models/services.model.js
+++ src/models/services.model.js
@@ -1,13 +1,13 @@
 const mysqlConnection = require("../../connection");
 
 const getServices = function(callback){
-    const sqlQuery = `WITH RECURSIVE category_path (id, title, path) AS
+    const sqlQuery = `WITH RECURSIVE category_path (id, services_id_parent_fk, title, path) AS
         (
-        SELECT id, title, title as path
+        SELECT id, services_id_parent_fk, title, title as path
             FROM services
             WHERE services_id_parent_fk IS NULL
         UNION ALL
-        SELECT c.id, c.title, CONCAT(cp.path, ' > ', c.title)
+        SELECT c.id, c.services_id_parent_fk, c.title, CONCAT(cp.path, ' > ', c.title)
             FROM category_path AS cp JOIN services AS c
             ON cp.id = c.services_id_parent_fk
         )
@@ -19,6 +19,22 @@ const getServices = function(callback){
         return callback(null,rows);
     });
 };
+
+// const getMainCategories = function(){
+//     const query = `SELECT
+//             id, title, description
+//         FROM
+//             category
+//         WHERE
+//         services_id_parent_fk IS NULL;`;
+//         mysqlConnection.query(query,(err, rows, fields)=>{
+//             if(!err){
+//                 return callback(null,rows);
+//             }else{
+//                 return callback(err);
+//             }
+//         });
+// }
 
 const getUserByCategory = function(parentID,callback){
     mysqlConnection.query("select * from services where services_id_parent_fk = "+parentID+";",(err, rows, fields)=>{
