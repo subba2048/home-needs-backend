20c8b47d0216780380583af64802b474bc3e3f83
JustVic009 <61919932+JustVic009@users.noreply.github.com>
2020-04-18 23:46:43 -0500

    updated to fix issues

--- src/routes/SOSchedule.routes.js
+++ src/routes/SOSchedule.routes.js
@@ -1,30 +1,31 @@
 const express = require("express");
 const Router = express.Router();
-const scheduleModel = require("../models/SOSchedule.model");
+const scheduleModel = require("../models/SOschedule.model");
 
-Router.get("/", (req, res, next) => {
-    scheduleModel.getSOSchedule(function(err,rows) {
-     if (!err)
+//router
+Router.get("/", (req, res,next) => {
+    scheduleModel.getsoschedule(function(err,rows) {
+     if (!err) 
      res.send(rows);
-     else 
+     else
      res.send(err);
 
-    }
-    )});
-
+    })
+});
+    //router
     Router.post("/update/:id",(req,res,next) => {
         const payload = req.body;
-        scheduleModel.updateSOSchedule(req.params.id,payload,function(err,rows) {
+        scheduleModel.updatesoschedule(req.params.id,payload,function(err,rows) {
             if (!err)
             res.send(rows);
             else 
             res.send(err);
         })
-    })
-
+    });
+    //router
     Router.post("/create/:id", (req,res,next) => {
         const payload = req.body; 
-        scheduleModel.createSOSchedule(payload, function(err,rows) {
+        scheduleModel.createsoschedule(payload, function(err,rows) {
             if (!err) {
             res.send(rows);
             
