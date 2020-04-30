772e6a9ff70b1f40ac398bbe9cb5e8392c391d3a
Subba <Subba@gmail.com>
2020-04-30 11:02:20 -0500

    db config changed

--- config/db.config.js
+++ config/db.config.js
@@ -3,9 +3,11 @@ var config = {
     database: {
         host: "localhost",
         user: "root",
-        password: "joshua7Aman",
+        password: "homeNeedsApp!T8",
         database: "home_needs_services_t8_v2"
     }
 };
 
 module.exports = config;
+
+// ALTER USER 'root'@'localhost' IDENTIFIED BY 'homeNeedsApp!T8';
