823c1358d5d08b617258cd1c0ecd265d70db8104
Subba <Subba@gmail.com>
2020-04-30 19:20:37 -0500

    changed route to /api

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 
 app.use(cors());
 
-app.use('/', indexRouter);
+app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
